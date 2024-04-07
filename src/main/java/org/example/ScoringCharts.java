39903ca9aac66568cd9b4beaaea0c841286e2615
0xc60f <0xc60f@duck.com>
2024-04-07 15:07:22 -0500

    Fixed compile error

--- src/main/java/org/example/ScoringCharts.java
+++ src/main/java/org/example/ScoringCharts.java
@@ -3,11 +3,11 @@ package org.example;
 public class ScoringCharts {
     //private Hashmap <String, Integer> wildLifescore;
 
-    public int calcWildlifeScore{
-        return;
+    public int calcWildlifeScore(Player p){
+        return 0;
     }
     public int calcTotalScore(Player p) {
         //return totalScore;
-        return;
+        return 0;
     }
 }
